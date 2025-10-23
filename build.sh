bd9b88ab426c8a012378c8ca5349c9c9c734236a
Dinesh-Kumar-Tunguturi <dineshkumartunguturi@gamil.com>
2025-10-23 11:34:21 +0530

    fix: Correct Vercel build configuration

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Exit on error
 set -o errexit
 
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3.9 manage.py collectstatic --no-input
+python3.9 manage.py migrate
